50f4d7c8aed5eb74f794c179a927b9b41c8c43f0
dragon <dai.yamamoto324@hotmail.com>
2017-07-17 18:33:30 +0300

    update database settings and app setting

--- config/database.php
+++ config/database.php
@@ -69,12 +69,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '35.194.139.48'),
             'port' => env('DB_PORT', '3306'),
-            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', 'egw_max'),
+            'username' => env('DB_USERNAME', 'max'),
+            'password' => env('DB_PASSWORD', 'max1273!'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
